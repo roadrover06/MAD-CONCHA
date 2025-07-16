449942efa761440cde3d4f822091d110dca01313
Primo <anabelle.velasquez@sdca.edu.ph>
2025-07-16 21:07:12 +0800

    COMISSION

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC9g7v9J6j5hvkq_hskn0Rvk2kM2CU8ApM",
-  authDomain: "carwashdetailing-efd04.firebaseapp.com",
-  projectId: "carwashdetailing-efd04",
-  storageBucket: "carwashdetailing-efd04.firebasestorage.app",
-  messagingSenderId: "431395218137",
-  appId: "1:431395218137:web:dcc01f28e2eb785e2414c8"
+  apiKey: "AIzaSyDjLtsulXhW7Xn2suLXLQowZt8BN0pVOg8",
+  authDomain: "mad-concha.firebaseapp.com",
+  projectId: "mad-concha",
+  storageBucket: "mad-concha.firebasestorage.app",
+  messagingSenderId: "948940304124",
+  appId: "1:948940304124:web:9a1bc6caad9d9616d7f179"
 };
 
 const app = initializeApp(firebaseConfig);
